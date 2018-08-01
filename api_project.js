c83d69244ac27491b44a29017a896c710498e16d
kslsoumya <37289248+kslsoumya@users.noreply.github.com>
2018-08-01 13:59:48 +0530

    Add files via upload

--- api_project.js
+++ api_project.js
@@ -1,15 +1,15 @@
 define({
-  "name": "Group Chat Application",
+  "name": "Group Chat App",
   "version": "0.0.1",
-  "description": "Documentation for Group Chat Application",
-  "title": "Group Chat Application",
-  "url": "http://localhost:3000",
+  "description": "Documentation for Group Chat App",
+  "title": "Group Chat App",
+  "url": "http://chat-api.themeanstackpro.com",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-07-24T14:58:36.795Z",
+    "time": "2018-08-01T07:42:06.985Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
